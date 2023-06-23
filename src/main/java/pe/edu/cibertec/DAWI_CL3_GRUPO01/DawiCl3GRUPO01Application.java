3a994eb5d55ec3b83aa7535cd4c3f4ce893677e8
jdanielq <quesadajuandaniel@gmail.com>
2023-06-23 10:17:13 -0500

    Inclusion de paquetes

---
+++ src/main/java/pe/edu/cibertec/DAWI_CL3_GRUPO01/DawiCl3GRUPO01Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWI_CL3_GRUPO01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl3GRUPO01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl3GRUPO01Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DAWI_CL3_Quesada/DawiCl3QuesadaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWI_CL3_Quesada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl3QuesadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
